00146cba083d61f0d19e8c821b8307b24364d8b2
Jonathan Han <jonweshan@gmail.com>
2024-06-11 13:25:49 -0700

    Add CI integration

--- src/cmd/shardctrler-server/main_server.go
+++ src/cmd/shardctrler-server/main_server.go
@@ -3,17 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
-	"project-root/labrpc"
-	"project-root/shardctrler/server"
+	"net"
+    "net/rpc"
+    "strconv"
+	"encoding/gob"
+    "cpsc416/shardctrler"
+	"cpsc416/raft"
+	"cpsc416/kvsRPC"
 )
 
+// TODO: Add ServiceIP and server_list to compose
 func main() {
-	serviceName := os.Getenv("SERVICE_NAME")
-	servicePort := os.Getenv("SERVICE_PORT")
+    serviceName := os.Getenv("SERVICE_NAME")
+    servicePort := os.Getenv("SERVICE_PORT")
+	serverList := os.Getenv("SERVER_LIST")
+    address := fmt.Sprintf(":%s", servicePort)
 
-	fmt.Printf("Starting %s on port %s\n", serviceName, servicePort)
+    fmt.Printf("Starting %s on port %s\n", serviceName, servicePort)
 
-	network := labrpc.MakeNetwork()
-	srv := server.MakeServer() // Assuming MakeServer() is a function in server package
-	network.AddServer(serviceName, srv)
+	servers := make([]kvsRPC.RPCClient, len(serverList))
+	for idx, server := range serverList {
+		servers[idx] = kvsRPC.NewLabRPCClient(server)
+	}
+
+    thisIndex = strvconv.Atoi(serviceName[len(serviceName) - 1])
+
+	gob.Register(shardctrler.Op{})
+
+    // Create a new instance of server
+    srv := shardctrler.StartServer(servers, thisIndex, raft.MakePersister())
+
+    // Register the server with net/rpc
+    rpc.Register(srv)
+
+    // Start listening for incoming connections
+    listener, err := net.Listen("tcp", address)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+        return
+    }
+	defer listener.Close()
+
+    fmt.Printf("Server %s started on port %s\n", serviceName, servicePort)
+	rpc.Accept(listener)
 }
